2f9d9be7168cebee754520e6fb7875eeca6493cd
Araleness <denisovseriy@yandex.ru>
2025-07-09 16:03:42 +0300

    123

---
+++ app/build/kspCaches/debug/backups/java/byRounds/1/com/taskmanager/data/local/TaskDao_Impl.java
@@ -0,0 +1,475 @@
+package com.taskmanager.data.local;
+
+import android.database.Cursor;
+import android.os.CancellationSignal;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.SharedSQLiteStatement;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import com.taskmanager.domain.model.Task;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.IllegalStateException;
+import java.lang.Integer;
+import java.lang.Long;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import javax.annotation.processing.Generated;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+import kotlinx.coroutines.flow.Flow;
+
+@Generated("androidx.room.RoomProcessor")
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class TaskDao_Impl implements TaskDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<Task> __insertionAdapterOfTask;
+
+  private final Converters __converters = new Converters();
+
+  private final EntityDeletionOrUpdateAdapter<Task> __deletionAdapterOfTask;
+
+  private final EntityDeletionOrUpdateAdapter<Task> __updateAdapterOfTask;
+
+  private final SharedSQLiteStatement __preparedStmtOfDeleteTaskById;
+
+  private final SharedSQLiteStatement __preparedStmtOfUpdateTaskSequence;
+
+  public TaskDao_Impl(@NonNull final RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfTask = new EntityInsertionAdapter<Task>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "INSERT OR REPLACE INTO `tasks` (`id`,`title`,`description`,`dueDate`,`priority_value`,`isCompleted`,`sequence`,`createdAt`,`updatedAt`) VALUES (nullif(?, 0),?,?,?,?,?,?,?,?)";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final Task entity) {
+        statement.bindLong(1, entity.getId());
+        statement.bindString(2, entity.getTitle());
+        statement.bindString(3, entity.getDescription());
+        final Long _tmp = __converters.dateToTimestamp(entity.getDueDate());
+        if (_tmp == null) {
+          statement.bindNull(4);
+        } else {
+          statement.bindLong(4, _tmp);
+        }
+        statement.bindLong(5, entity.getPriorityValue());
+        final int _tmp_1 = entity.isCompleted() ? 1 : 0;
+        statement.bindLong(6, _tmp_1);
+        statement.bindLong(7, entity.getSequence());
+        final Long _tmp_2 = __converters.dateToTimestamp(entity.getCreatedAt());
+        if (_tmp_2 == null) {
+          statement.bindNull(8);
+        } else {
+          statement.bindLong(8, _tmp_2);
+        }
+        final Long _tmp_3 = __converters.dateToTimestamp(entity.getUpdatedAt());
+        if (_tmp_3 == null) {
+          statement.bindNull(9);
+        } else {
+          statement.bindLong(9, _tmp_3);
+        }
+      }
+    };
+    this.__deletionAdapterOfTask = new EntityDeletionOrUpdateAdapter<Task>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "DELETE FROM `tasks` WHERE `id` = ?";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final Task entity) {
+        statement.bindLong(1, entity.getId());
+      }
+    };
+    this.__updateAdapterOfTask = new EntityDeletionOrUpdateAdapter<Task>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "UPDATE OR ABORT `tasks` SET `id` = ?,`title` = ?,`description` = ?,`dueDate` = ?,`priority_value` = ?,`isCompleted` = ?,`sequence` = ?,`createdAt` = ?,`updatedAt` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final Task entity) {
+        statement.bindLong(1, entity.getId());
+        statement.bindString(2, entity.getTitle());
+        statement.bindString(3, entity.getDescription());
+        final Long _tmp = __converters.dateToTimestamp(entity.getDueDate());
+        if (_tmp == null) {
+          statement.bindNull(4);
+        } else {
+          statement.bindLong(4, _tmp);
+        }
+        statement.bindLong(5, entity.getPriorityValue());
+        final int _tmp_1 = entity.isCompleted() ? 1 : 0;
+        statement.bindLong(6, _tmp_1);
+        statement.bindLong(7, entity.getSequence());
+        final Long _tmp_2 = __converters.dateToTimestamp(entity.getCreatedAt());
+        if (_tmp_2 == null) {
+          statement.bindNull(8);
+        } else {
+          statement.bindLong(8, _tmp_2);
+        }
+        final Long _tmp_3 = __converters.dateToTimestamp(entity.getUpdatedAt());
+        if (_tmp_3 == null) {
+          statement.bindNull(9);
+        } else {
+          statement.bindLong(9, _tmp_3);
+        }
+        statement.bindLong(10, entity.getId());
+      }
+    };
+    this.__preparedStmtOfDeleteTaskById = new SharedSQLiteStatement(__db) {
+      @Override
+      @NonNull
+      public String createQuery() {
+        final String _query = "DELETE FROM tasks WHERE id = ?";
+        return _query;
+      }
+    };
+    this.__preparedStmtOfUpdateTaskSequence = new SharedSQLiteStatement(__db) {
+      @Override
+      @NonNull
+      public String createQuery() {
+        final String _query = "UPDATE tasks SET sequence = ? WHERE id = ?";
+        return _query;
+      }
+    };
+  }
+
+  @Override
+  public Object insertTask(final Task task, final Continuation<? super Long> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Long>() {
+      @Override
+      @NonNull
+      public Long call() throws Exception {
+        __db.beginTransaction();
+        try {
+          final Long _result = __insertionAdapterOfTask.insertAndReturnId(task);
+          __db.setTransactionSuccessful();
+          return _result;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object deleteTask(final Task task, final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __deletionAdapterOfTask.handle(task);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object updateTask(final Task task, final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __updateAdapterOfTask.handle(task);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object deleteTaskById(final long taskId, final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        final SupportSQLiteStatement _stmt = __preparedStmtOfDeleteTaskById.acquire();
+        int _argIndex = 1;
+        _stmt.bindLong(_argIndex, taskId);
+        try {
+          __db.beginTransaction();
+          try {
+            _stmt.executeUpdateDelete();
+            __db.setTransactionSuccessful();
+            return Unit.INSTANCE;
+          } finally {
+            __db.endTransaction();
+          }
+        } finally {
+          __preparedStmtOfDeleteTaskById.release(_stmt);
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object updateTaskSequence(final long taskId, final int sequence,
+      final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        final SupportSQLiteStatement _stmt = __preparedStmtOfUpdateTaskSequence.acquire();
+        int _argIndex = 1;
+        _stmt.bindLong(_argIndex, sequence);
+        _argIndex = 2;
+        _stmt.bindLong(_argIndex, taskId);
+        try {
+          __db.beginTransaction();
+          try {
+            _stmt.executeUpdateDelete();
+            __db.setTransactionSuccessful();
+            return Unit.INSTANCE;
+          } finally {
+            __db.endTransaction();
+          }
+        } finally {
+          __preparedStmtOfUpdateTaskSequence.release(_stmt);
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Flow<List<Task>> getAllTasks() {
+    final String _sql = "SELECT * FROM tasks ORDER BY dueDate ASC";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return CoroutinesRoom.createFlow(__db, false, new String[] {"tasks"}, new Callable<List<Task>>() {
+      @Override
+      @NonNull
+      public List<Task> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfTitle = CursorUtil.getColumnIndexOrThrow(_cursor, "title");
+          final int _cursorIndexOfDescription = CursorUtil.getColumnIndexOrThrow(_cursor, "description");
+          final int _cursorIndexOfDueDate = CursorUtil.getColumnIndexOrThrow(_cursor, "dueDate");
+          final int _cursorIndexOfPriorityValue = CursorUtil.getColumnIndexOrThrow(_cursor, "priority_value");
+          final int _cursorIndexOfIsCompleted = CursorUtil.getColumnIndexOrThrow(_cursor, "isCompleted");
+          final int _cursorIndexOfSequence = CursorUtil.getColumnIndexOrThrow(_cursor, "sequence");
+          final int _cursorIndexOfCreatedAt = CursorUtil.getColumnIndexOrThrow(_cursor, "createdAt");
+          final int _cursorIndexOfUpdatedAt = CursorUtil.getColumnIndexOrThrow(_cursor, "updatedAt");
+          final List<Task> _result = new ArrayList<Task>(_cursor.getCount());
+          while (_cursor.moveToNext()) {
+            final Task _item;
+            final long _tmpId;
+            _tmpId = _cursor.getLong(_cursorIndexOfId);
+            final String _tmpTitle;
+            _tmpTitle = _cursor.getString(_cursorIndexOfTitle);
+            final String _tmpDescription;
+            _tmpDescription = _cursor.getString(_cursorIndexOfDescription);
+            final Date _tmpDueDate;
+            final Long _tmp;
+            if (_cursor.isNull(_cursorIndexOfDueDate)) {
+              _tmp = null;
+            } else {
+              _tmp = _cursor.getLong(_cursorIndexOfDueDate);
+            }
+            _tmpDueDate = __converters.fromTimestamp(_tmp);
+            final int _tmpPriorityValue;
+            _tmpPriorityValue = _cursor.getInt(_cursorIndexOfPriorityValue);
+            final boolean _tmpIsCompleted;
+            final int _tmp_1;
+            _tmp_1 = _cursor.getInt(_cursorIndexOfIsCompleted);
+            _tmpIsCompleted = _tmp_1 != 0;
+            final int _tmpSequence;
+            _tmpSequence = _cursor.getInt(_cursorIndexOfSequence);
+            final Date _tmpCreatedAt;
+            final Long _tmp_2;
+            if (_cursor.isNull(_cursorIndexOfCreatedAt)) {
+              _tmp_2 = null;
+            } else {
+              _tmp_2 = _cursor.getLong(_cursorIndexOfCreatedAt);
+            }
+            final Date _tmp_3 = __converters.fromTimestamp(_tmp_2);
+            if (_tmp_3 == null) {
+              throw new IllegalStateException("Expected NON-NULL 'java.util.Date', but it was NULL.");
+            } else {
+              _tmpCreatedAt = _tmp_3;
+            }
+            final Date _tmpUpdatedAt;
+            final Long _tmp_4;
+            if (_cursor.isNull(_cursorIndexOfUpdatedAt)) {
+              _tmp_4 = null;
+            } else {
+              _tmp_4 = _cursor.getLong(_cursorIndexOfUpdatedAt);
+            }
+            final Date _tmp_5 = __converters.fromTimestamp(_tmp_4);
+            if (_tmp_5 == null) {
+              throw new IllegalStateException("Expected NON-NULL 'java.util.Date', but it was NULL.");
+            } else {
+              _tmpUpdatedAt = _tmp_5;
+            }
+            _item = new Task(_tmpId,_tmpTitle,_tmpDescription,_tmpDueDate,_tmpPriorityValue,_tmpIsCompleted,_tmpSequence,_tmpCreatedAt,_tmpUpdatedAt);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @Override
+  public Object getTaskById(final long taskId, final Continuation<? super Task> $completion) {
+    final String _sql = "SELECT * FROM tasks WHERE id = ?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    _statement.bindLong(_argIndex, taskId);
+    final CancellationSignal _cancellationSignal = DBUtil.createCancellationSignal();
+    return CoroutinesRoom.execute(__db, false, _cancellationSignal, new Callable<Task>() {
+      @Override
+      @Nullable
+      public Task call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfTitle = CursorUtil.getColumnIndexOrThrow(_cursor, "title");
+          final int _cursorIndexOfDescription = CursorUtil.getColumnIndexOrThrow(_cursor, "description");
+          final int _cursorIndexOfDueDate = CursorUtil.getColumnIndexOrThrow(_cursor, "dueDate");
+          final int _cursorIndexOfPriorityValue = CursorUtil.getColumnIndexOrThrow(_cursor, "priority_value");
+          final int _cursorIndexOfIsCompleted = CursorUtil.getColumnIndexOrThrow(_cursor, "isCompleted");
+          final int _cursorIndexOfSequence = CursorUtil.getColumnIndexOrThrow(_cursor, "sequence");
+          final int _cursorIndexOfCreatedAt = CursorUtil.getColumnIndexOrThrow(_cursor, "createdAt");
+          final int _cursorIndexOfUpdatedAt = CursorUtil.getColumnIndexOrThrow(_cursor, "updatedAt");
+          final Task _result;
+          if (_cursor.moveToFirst()) {
+            final long _tmpId;
+            _tmpId = _cursor.getLong(_cursorIndexOfId);
+            final String _tmpTitle;
+            _tmpTitle = _cursor.getString(_cursorIndexOfTitle);
+            final String _tmpDescription;
+            _tmpDescription = _cursor.getString(_cursorIndexOfDescription);
+            final Date _tmpDueDate;
+            final Long _tmp;
+            if (_cursor.isNull(_cursorIndexOfDueDate)) {
+              _tmp = null;
+            } else {
+              _tmp = _cursor.getLong(_cursorIndexOfDueDate);
+            }
+            _tmpDueDate = __converters.fromTimestamp(_tmp);
+            final int _tmpPriorityValue;
+            _tmpPriorityValue = _cursor.getInt(_cursorIndexOfPriorityValue);
+            final boolean _tmpIsCompleted;
+            final int _tmp_1;
+            _tmp_1 = _cursor.getInt(_cursorIndexOfIsCompleted);
+            _tmpIsCompleted = _tmp_1 != 0;
+            final int _tmpSequence;
+            _tmpSequence = _cursor.getInt(_cursorIndexOfSequence);
+            final Date _tmpCreatedAt;
+            final Long _tmp_2;
+            if (_cursor.isNull(_cursorIndexOfCreatedAt)) {
+              _tmp_2 = null;
+            } else {
+              _tmp_2 = _cursor.getLong(_cursorIndexOfCreatedAt);
+            }
+            final Date _tmp_3 = __converters.fromTimestamp(_tmp_2);
+            if (_tmp_3 == null) {
+              throw new IllegalStateException("Expected NON-NULL 'java.util.Date', but it was NULL.");
+            } else {
+              _tmpCreatedAt = _tmp_3;
+            }
+            final Date _tmpUpdatedAt;
+            final Long _tmp_4;
+            if (_cursor.isNull(_cursorIndexOfUpdatedAt)) {
+              _tmp_4 = null;
+            } else {
+              _tmp_4 = _cursor.getLong(_cursorIndexOfUpdatedAt);
+            }
+            final Date _tmp_5 = __converters.fromTimestamp(_tmp_4);
+            if (_tmp_5 == null) {
+              throw new IllegalStateException("Expected NON-NULL 'java.util.Date', but it was NULL.");
+            } else {
+              _tmpUpdatedAt = _tmp_5;
+            }
+            _result = new Task(_tmpId,_tmpTitle,_tmpDescription,_tmpDueDate,_tmpPriorityValue,_tmpIsCompleted,_tmpSequence,_tmpCreatedAt,_tmpUpdatedAt);
+          } else {
+            _result = null;
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+          _statement.release();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object getHighestSequence(final Continuation<? super Integer> $completion) {
+    final String _sql = "SELECT MAX(sequence) FROM tasks";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    final CancellationSignal _cancellationSignal = DBUtil.createCancellationSignal();
+    return CoroutinesRoom.execute(__db, false, _cancellationSignal, new Callable<Integer>() {
+      @Override
+      @Nullable
+      public Integer call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final Integer _result;
+          if (_cursor.moveToFirst()) {
+            final Integer _tmp;
+            if (_cursor.isNull(0)) {
+              _tmp = null;
+            } else {
+              _tmp = _cursor.getInt(0);
+            }
+            _result = _tmp;
+          } else {
+            _result = null;
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+          _statement.release();
+        }
+      }
+    }, $completion);
+  }
+
+  @NonNull
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
